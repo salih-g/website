a8d36e2a8a6e454db5d5e4f65f129d8714522e33
Salih Gumus <sudanmerinosu@gmail.com>
2021-04-16 01:06:39 +0300

    first commit

---
+++ src/views/Gallery.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="Gallery">
+    <h1 class="title">Inside my world</h1>
+
+    <div class="photo-grid">
+      <div
+        class="card card-tall"
+        style="background-image:url('https://drive.google.com/uc?id=1q3LFPywJV-cB9B_HjrpAJhSz506dBZC3')"
+      ></div>
+      <div
+        class="card card-tall"
+        style="background-image:url('https://drive.google.com/uc?id=1n4EIi73kmXluHiXgat40v9SdCMeHkdE6')"
+      ></div>
+      <div
+        class="card card-wide"
+        style="background-image:url('https://drive.google.com/uc?id=1U1lxuPkUw_hChEqhnVku-ADCr0m89AaL')"
+      ></div>
+
+      <div
+        class="card card-tall"
+        style="background-image:url('https://drive.google.com/uc?id=1cozgceIALzGNNI-QMmHcP7d6wWnAFxpy')"
+      ></div>
+      <div
+        class="card card-tall"
+        style="background-image:url('https://drive.google.com/uc?id=1gUiVYFQS4WEKIpAIQVe8knikpMbLsQkM')"
+      ></div>
+      <div
+        class="card card-wide card-tall"
+        style="background-image:url('https://drive.google.com/uc?id=16s436l2TW4mfFwtYqPbzIIVIBzZsn2Lm')"
+      ></div>
+      <div
+        class="card "
+        style="background-image:url('https://drive.google.com/uc?id=1NL92vWNb5dEy5F1jatzIfQGpLlWLiJxJ')"
+      ></div>
+
+      <div
+        class="card  card-wide"
+        style="background-image:url('https://drive.google.com/uc?id=1JzL2f9wCFUkE05za29L_Z-qWv1yfzqqs')"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+};
+</script>
+
+<style>
+.title {
+  display: flex;
+  justify-content: center;
+  font-size: 48px;
+}
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #353535;
+  font-size: 3rem;
+  color: #fff;
+  box-shadow: rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem, rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem;
+  height: 100%;
+  width: 100%;
+  border-radius: 4px;
+  transition: all 500ms;
+  overflow: hidden;
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.card:hover {
+  box-shadow: rgba(2, 8, 20, 0.1) 0px 0.35em 1.175em, rgba(2, 8, 20, 0.08) 0px 0.175em 0.5em;
+  transform: translateY(-3px) scale(1.1);
+}
+.photo-grid {
+  display: grid;
+  gap: 1rem;
+
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  grid-auto-rows: 240px;
+}
+@media screen and (min-width: 600px) {
+  .card-tall {
+    grid-row: span 2 / auto;
+  }
+  .card-wide {
+    grid-column: span 2 / auto;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,175 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div class="grid-container">
+      <div class="home">
+        <div class="page">
+          <div class="picture"><img src="../assets/images/header.jpg" alt="" /></div>
+          <div class="title">
+            <div class="text">
+              <p><b>Salih </b>Gümüş</p>
+              <span>Developer </span>/
+              <span> Amateur Photographer</span>
+            </div>
+          </div>
+          <div class="hello"><h1 class="hello">Hello.</h1></div>
+          <div class="description">
+            <p class="parag">
+              I'm a Computer Engineering student. I'm mid-level Full-Stack developer and junior game
+              developer. Also I'm an amateur photographer.
+            </p>
+            <div class="address">Adana/Turkey</div>
+          </div>
+          <div class="footer">
+            <div class="left-icons">
+              <a href="https://www.instagram.com/sudanmerinosu/" target="_blank">
+                <i class="im im-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/salihgumus/" target="_blank">
+                <i class="im im-linkedin" />
+              </a>
+            </div>
+            <div class="right-icons">
+              <a href="https://twitter.com/sudanmerinosu" target="_blank">
+                <i class="im im-twitter" />
+              </a>
+              <a href="https://github.com/sudanmerinosu" target="_blank">
+                <i class="im im-github"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Kalam:wght@300&display=swap');
+
+.main {
+  display: flex;
+  justify-content: center;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 0px 0px;
+  grid-template-areas:
+    'home home home home'
+    'home home home home'
+    'home home home home';
+}
+
+.home {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(9, 1fr);
+  gap: 0px 0px;
+  grid-template-areas:
+    '. . . . . '
+    ' page page page page '
+    ' page page page page '
+    ' page page page page '
+    ' page page page page '
+    ' page page page page '
+    ' page page page page '
+    ' page page page page '
+    '. . . . . ';
+  grid-area: home;
+}
+
+.page {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  gap: 0px 0px;
+  grid-area: page;
+
+  margin-top: 40px;
+
+  background-color: white;
+  -webkit-box-shadow: 0px 0px 50px -15px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 0px 50px -15px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 0px 50px -15px rgba(0, 0, 0, 0.75);
+}
+
+.picture {
+  grid-area: 2 / 3 / 5 / 6;
+  z-index: 1;
+}
+
+.title {
+  max-width: 100%;
+  grid-area: 3 / 2 / 6 / 5;
+  background-color: #e8e8e8;
+  position: relative;
+}
+.text {
+  position: absolute;
+  top: 200px;
+  left: 30px;
+  letter-spacing: 2px;
+  line-height: 15px;
+  font-family: 'Oswald', sans-serif;
+}
+.text p {
+  font-size: 24px;
+}
+
+.hello {
+  max-width: 100%;
+  grid-area: 6 / 1 / 7 / 3;
+  font-family: Kalam;
+  font-size: 42px;
+  padding-left: 10px;
+  padding-top: 2px;
+}
+.description {
+  grid-area: 7 / 1 / 8 / 7;
+  position: relative;
+}
+.parag {
+  font-family: Kalam;
+  font-size: 18px;
+  font-weight: 400;
+  position: absolute;
+  bottom: 15px;
+  padding: 30px;
+}
+.address {
+  position: absolute;
+  bottom: 0px;
+  left: 225px;
+  font-size: 24px;
+}
+.footer {
+  grid-area: 8 / 1 / 9 / 7;
+  position: relative;
+}
+.footer a {
+  padding: 20px;
+  color: black;
+}
+.left-icons {
+  position: absolute;
+  bottom: 10px;
+  left: 225px;
+}
+.right-icons {
+  position: absolute;
+  bottom: 50px;
+  left: 225px;
+}
+
+@media only screen and (max-width: 820) {
+}
+</style>
